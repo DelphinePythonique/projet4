7f4be69de173344aaa2987d46f571922907fa378
DelphinePythonique <delphine.lemire+openclassroom@gmail.com>
2022-04-26 12:11:44 +0200

    add quick_debug.py in order to test features quickly

--- quick_debug.py
+++ quick_debug.py
@@ -16,10 +16,7 @@ player10 = Player("joueurB-20", "Delphine", "01/01/1970", "feminine", 20)
 player11 = Player("joueurB-50", "Jean", "01/01/1971", "masculine", 50)
 player12 = Player("joueurD-30", "Remi", "01/01/1971", "masculine", 30)
 
-'''
-for p in Player.list_of_players_by_ranking_sort():
-    print(p)
-    '''
+
 
 tournament = Tournament("tournament of 20/05/2022", "Rouen", "20/05/2022", "20/05/2022", "blitz")
 tournament.add_player(player1)
@@ -30,17 +27,28 @@ tournament.add_player(player5)
 tournament.add_player(player6)
 tournament.add_player(player7)
 tournament.add_player(player8)
-
 tournament.generate_round()
 for round in tournament.rounds:
     print(round)
 
+print("list of player sort by ranking, alphabetic")
+for player in tournament.extract_tournament_player_sort_by_ranking():
+    print(f"id:{player.identifiant}({player.surname}): ranking:{player.ranking}")
+
 tournament.rounds[0].do_the_paring_by_ranking()
+
+print("round #0 - Matchs")
+for match in tournament.rounds[0].matchs:
+    print(f"player #1:{match[0][0].identifiant} / player #2:{match[1][0].identifiant} ")
+
 tournament.rounds[0].set_match_equality(0)
 tournament.rounds[0].set_player_one_is_winner(1)
 tournament.rounds[0].set_player_two_is_winner(2)
 tournament.rounds[0].set_player_one_is_winner(3)
+print("list of player sort by result last matchs")
 for player, result in tournament.extract_tournament_player_sort_by_result():
-    print(f"{player.identifiant}:{result}")
+    print(f"id:{player.identifiant}({player.surname}): result:{result}")
+print("round #1 - Matchs")
 tournament.rounds[1].do_the_paring_by_total_rounds_result()
-print(tournament.rounds[1].matchs)
+for match in tournament.rounds[1].matchs:
+    print(f"player #1:{match[0][0].identifiant} / player #2:{match[1][0].identifiant} ")
